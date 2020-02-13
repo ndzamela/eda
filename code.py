97cda1489d78292766d1dbbdbe84514c8af3c29a
ndzamela <ndzamela@users.noreply.github.com>
2020-02-13 08:52:45 +0200

    Code file created.

---
+++ code.py
@@ -0,0 +1,50 @@
+# --------------
+# Code starts here
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#### Data 1
+# Load the data
+
+api = pd.read_csv(path)
+
+# Overview of the data
+print(api.info())
+
+print(api.describe())
+
+# Histogram showing distribution of car prices
+
+
+# Countplot of the make column
+
+
+# Jointplot showing relationship between 'horsepower' and 'price' of the car
+
+
+# Correlation heat map
+
+
+# boxplot that shows the variability of each 'body-style' with respect to the 'price'
+
+
+#### Data 2
+
+# Load the data
+
+
+# Impute missing values with mean
+
+
+# Skewness of numeric features
+
+
+
+# Label encode 
+
+
+
+# Code ends here
+
+
